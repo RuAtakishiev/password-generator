e027d869475040b1445b085791d3a5e4e202a9d9
RuAtakishiev <atakishiev32ruslan@gmail.com>
2023-09-25 21:40:25 +0300

    Added data collection from the first page

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,67 @@
 'use strict';
 
-function generatePwds() {
-    window.location.href = 'gen_pwds.html';
+function generateObject(objectName, requestedNodeList) {
+    try {
+        let objectKeys;
+        let objectValues;
+        let resultObject = {};
+    
+        if (objectName == "pwds_length_quantity") {
+            for (let piIndex = 0; piIndex < 2; piIndex++) {
+                objectKeys = requestedNodeList[piIndex].id;
+                objectValues = requestedNodeList[piIndex].value;
+                
+                Object.assign(resultObject, {[objectKeys]: objectValues});
+            }
+        } else if (objectName == "alphabet_composition") {
+            for (let acIndex = 0; acIndex < 4; acIndex++) {
+                objectKeys = requestedNodeList[acIndex].id;
+                objectValues = requestedNodeList[acIndex].checked;
+    
+                Object.assign(resultObject, {[objectKeys]: objectValues});
+            }
+        } else {
+            return False
+        }
+    
+        return resultObject
+    } catch (ReferenceError) {
+        console.error(`Unable to generate an object! Unknown object name ${objectName}!\nExcpected: "pwds_length_quantity" or "alphabet_composition"\n${ReferenceError}`)
+    }
+    
 };
+
+function getJSONPwdsInfo() {
+    const pwdsLengthQuantity = generateObject("pwds_length_quantity", document.querySelectorAll('.text-input'));
+    const alphabetComposition = generateObject("alphabet_composition", document.querySelectorAll('.chk-btn'));
+
+    const json = JSON.stringify([pwdsLengthQuantity, alphabetComposition]);
+
+    return json
+};
+
+async function getRequest() {
+    try {
+        const jsonData = getJSONPwdsInfo();
+        
+        window.location.href = 'gen_pwds.html';
+
+        console.log("Успех:", jsonData);
+    } catch (e) {
+        return console.log(`Generation failed: ${e}`);
+    }
+}
 
 function goBack() {
     window.location.href = 'index.html';
 };
 
-function getSnackbar(snackbarMessage, snackbarColor) {
+function getSnackbar(snackbarMessage) {
     let snackbar = document.getElementById('snackbar');
 
     snackbar.className = 'show';
-
-    snackbar.style.backgroundColor = snackbarColor;
-
-    // if snackbar color is GREEN
-    if (snackbarColor == '#28A745') {
-        snackbar.innerText = `${snackbarMessage} copied!`;
-    } else {
-        snackbar.innerText = `Copy failed: ${snackbarMessage}`;
-    }
+    snackbar.style.backgroundColor = '#28A745';
+    snackbar.innerText = `${snackbarMessage} copied!`;
 
     // After 1.2 seconds, remove the show class from snackbar
     setTimeout(function(){ snackbar.className = snackbar.className.replace('show', ''); }, 1200);
@@ -32,16 +73,13 @@ async function copyToClipboard(copyMessage, pwd) {
 
         console.log(`${copyMessage} was copied succesfully`);
 
-        getSnackbar(copyMessage, '#28A745');
+        getSnackbar(copyMessage);
     } catch(e) {
         console.error(`Copy failed: ${e}`);
-        
-        getSnackbar(e, '#DC3545');
     }
 };
 
 function removeActivePwdInputs(activePwdInputs) {
-    console.log('remove');
     activePwdInputs.forEach((activePwdInput) => {
         activePwdInput.classList.remove('active');
     })
@@ -65,13 +103,13 @@ function copyAll() {
 
 function copyThis(pwdElement) {
     let thisPwd = pwdElement.value;
-        let activePwdInputs = document.querySelectorAll('.active');
+    let activePwdInputs = document.querySelectorAll('.active');
 
-        if (activePwdInputs.length > 0) {
-            removeActivePwdInputs(activePwdInputs);
-        }
+    if (activePwdInputs.length > 0) {
+        removeActivePwdInputs(activePwdInputs);
+    }
 
-        copyToClipboard(thisPwd, thisPwd);
+    copyToClipboard(thisPwd, thisPwd);
 
-        pwdElement.classList.add('active');
+    pwdElement.classList.add('active');
 };
